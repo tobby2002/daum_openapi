e6295922439ade713fd38f274b89cd581aa60e53
tobby2002 <tobby2002@gmail.com>
2016-09-20 15:29:31 +0900

    python2 --> 3 changed

--- build/lib/daum_openapi/__init__.py
+++
@@ -1,8 +0,0 @@
-from json_object import json_object
-from common import request 
-from contents import contents
-from local import local
-from search import search
-from shopping import shopping
-from socialpick import socialpick
- 
--- build/lib/daum_openapi/json_object/__init__.py
+++
@@ -1,1 +0,0 @@
-from json_object import *
